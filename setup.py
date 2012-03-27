d83b34388b4d4e02fa3e8682111bc34301c23044
David Cramer <dcramer@gmail.com>
2012-03-27 14:44:50 -0700

    0.4.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ tests_require = [
 
 setup(
     name='nose-quickunit',
-    version='0.4.4',
+    version='0.4.5',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     description='A discovery plugin for Nose which relies on sane structure.',
