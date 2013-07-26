a585148d320778097c0c20bc9ce260720e4c135a
David Cramer <dcramer@gmail.com>
2013-07-26 15:55:22 -0700

    Detect git and hg via CLI commands

--- quickunit/vcs/base.py
+++ quickunit/vcs/base.py
@@ -1,4 +1,5 @@
 import os.path
+from subprocess import check_call, CalledProcessError
 
 from collections import namedtuple
 
@@ -10,8 +11,27 @@ def get_vcs(root):
     if os.path.exists(os.path.join(root, '.git')):
         import quickunit.vcs.git
         return quickunit.vcs.git
+
     elif os.path.exists(os.path.join(root, '.hg')):
         import quickunit.vcs.hg
         return quickunit.vcs.hg
+
+    DEVNULL = open('/dev/null')
+
+    try:
+        check_call('hg status', cwd=root, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    except CalledProcessError:
+        pass
     else:
-        raise NotImplementedError('We didnt try hard enough to figure out the vcs of %r' % (os.path.realpath(root),))
+        import quickunit.vcs.hg
+        return quickunit.vcs.hg
+
+    try:
+        check_call('git status', cwd=root, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    except CalledProcessError:
+        pass
+    else:
+        import quickunit.vcs.git
+        return quickunit.vcs.git
+
+    raise NotImplementedError('We didnt try hard enough to figure out the vcs of %r' % (os.path.realpath(root),))
